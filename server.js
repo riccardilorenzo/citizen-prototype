8962ca1d487b0960d60f7dbe8f207b89d6a49984
Lorenzo Riccardi <31043257+riccardilorenzo@users.noreply.github.com>
2023-05-09 13:23:46 +0200

    Optimization of AJAX requests, registration and login procedures

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const web3 = new Web3("https://eth-sepolia.g.alchemy.com/v2/" + process.env.API_
 const privateKey = process.env.PRIVATE_KEY
 const bufferedPrivateKey = Buffer.from(privateKey)
 
-//import { PassportReader } from 'verifiable-data'
 const vd = require('verifiable-data')
 const passportLogicAddress = process.env.PASSPORTLOGIC_ADDRESS
 const passportLogicRegistryAddress = process.env.PASSPORTLOGICREGISTRY_ADDRESS
@@ -89,7 +88,7 @@ async function retrieveAllMessages() {
                 if (!names.hasOwnProperty(fact.passportAddress)) {
                     names[fact.passportAddress] = await new vd.FactReader(web3, fact.passportAddress).getString(tesiEthereumAddress, "username")
                 }
-                if (fact.key != '') {       // First test messages in the blockchain did not saved correctly under the specified key
+                if (fact.key != '') {       // First test messages in the blockchain did not save correctly (under a empty key)
                     messages.push({
                         timestamp: +fact.key,
                         author: names[fact.passportAddress],
@@ -105,15 +104,6 @@ async function retrieveAllMessages() {
 function hash(string) {     // I could pass a second parameter as the used hash algorithm, for future-proofing (and saving the used algorithm in the blockchain)
   return createHash('sha256').update(string).digest('hex');
 }
-
-/*function formatUsername(username) {
-    //return Date.now() + "-" + username
-    return Date.now()
-}
-
-function parseKey() {
-    // WITH CURRENT LOGIC IT'S USELESS (SEE formatUsername())
-}*/
 
 function isAuthenticated(request) {
     if (request.session && request.session.logged && request.session.logged == true)
@@ -133,86 +123,67 @@ app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, 'web/register.html'));
 })
 
-app.post("/home", (req, res) => {       // Logic for creating passport or importing it and maintaining in session
+app.post("/home", async (req, res) => {       // Logic for creating passport or importing it and maintaining in session
     // Username must be unique in case of registration, so the user can avoid using its passport address and make it seamless with a normal chat
     if (req.session)
         req.session.logged = false
     
     if (req.body.todo == "Registrati") {
-        // after having created the digital identity, you MUST set the whitelist to only allow the creator of the D.I. to write to it
 
-        passportReader.getPassportsList(passportFactoryAddress).then(passports => {
-            let shouldStop = false
-            passports.forEach(function (pp) {
-                new vd.FactReader(web3, pp.passportAddress).getString(tesiEthereumAddress, "username").then(uname => {
-                    if (uname == req.body.username) {
-                        shouldStop = true
-                        res.redirect(409, '/register')
-                        return
-                    }
-                })
-            })
-            if (shouldStop) return
-            generator.createPassport(tesiEthereumAddress).then(txConf => {      // Need to change this pyramid with async/await functions
-                sendTransaction(txConf).then(receipt => {
-                    let passAddress = vd.PassportGenerator.getPassportAddressFromReceipt(receipt)
-                    console.log("Passport created at " + passAddress + "!")
-                    new vd.PassportOwnership(web3, passAddress).claimOwnership(tesiEthereumAddress).then(txC => {
-                        sendTransaction(txC).then(rec => {
-                            console.log("Passport ownership claimed!")
-                            new vd.Permissions(web3, passAddress).setWhitelistOnlyPermission(true, tesiEthereumAddress).then(txD => {
-                                sendTransaction(txD).then(rec => {
-                                    console.log("Passport whitelisted!")
-                                    let pWriter = new vd.FactWriter(web3, passAddress)
-                                    pWriter.setString("username", req.body.username, tesiEthereumAddress).then(tData => {
-                                        sendTransaction(tData).then(r => {
-                                            console.log("Passport username set!")
-                                            pWriter.setString("password", hash(req.body.password), tesiEthereumAddress).then(tConfiguration => {
-                                                sendTransaction(tConfiguration).then(passReceipt => {
-                                                    console.log("Passport password set, logging in...")
-                                                    req.session.username = req.body.username
-                                                    req.session.logged = true
-                                                    req.session.address = passAddress
-                                                    res.sendFile(path.join(__dirname, 'web/home.html'));
-                                                })
-                                            })
-                                        })
-                                    })
-                                })
-                            })
-                        })
-                    })
-                })
-            })
-        })
+        let passports = await passportReader.getPassportsList(passportFactoryAddress)
+        for (let i in passports) {
+            let uname = await new vd.FactReader(web3, passports[i].passportAddress).getString(tesiEthereumAddress, "username")
+            if (uname == req.body.username) {
+                res.redirect('/register')
+                return
+            }
+        }
+        let txConf = await generator.createPassport(tesiEthereumAddress)
+        let receipt = await sendTransaction(txConf)
+        let passAddress = vd.PassportGenerator.getPassportAddressFromReceipt(receipt)
+        console.log("Passport created at " + passAddress + "!")
+        txConf = await new vd.PassportOwnership(web3, passAddress).claimOwnership(tesiEthereumAddress)
+        receipt = await sendTransaction(txConf)
+        console.log("Passport ownership claimed!")
+        txConf = await new vd.Permissions(web3, passAddress).setWhitelistOnlyPermission(true, tesiEthereumAddress)
+        receipt = await sendTransaction(txConf)
+        console.log("Passport whitelisted!")
+        let pWriter = new vd.FactWriter(web3, passAddress)
+        txConf = await pWriter.setString("username", req.body.username, tesiEthereumAddress)
+        receipt = await sendTransaction(txConf)
+        console.log("Passport username set!")
+        txConf = await pWriter.setString("password", hash(req.body.password), tesiEthereumAddress)
+        receipt = await sendTransaction(txConf)
+        console.log("Passport password set, logging in...")
+
+        req.session.username = req.body.username
+        req.session.logged = true
+        req.session.address = passAddress
+
+        res.sendFile(path.join(__dirname, 'web/home.html'))
     } else if (req.body.todo == "Login") {
-        passportReader.getPassportsList(passportFactoryAddress).then(passports => {
+        passportReader.getPassportsList(passportFactoryAddress).then(async (passports) => {
             let found = false
-            //passports.forEach((pp) => {
             for (let i = 0; i < passports.length; i++) {
                 let psReader = new vd.FactReader(web3, passports[i].passportAddress)
-                psReader.getString(tesiEthereumAddress, "username").then(foundUsername => {
-                    if (foundUsername == req.body.username) {
-                        found = true
+                let foundUsername = await psReader.getString(tesiEthereumAddress, "username")
+                if (foundUsername == req.body.username) {
+                    found = true
 
-                        // Comparing password hashes
-                        psReader.getString(tesiEthereumAddress, "password").then(foundPassword => {
-                            if (hash(req.body.password) == foundPassword) {
-                                req.session.logged = true
-                                req.session.username = req.body.username
-                                req.session.address = passports[i].passportAddress
-                                res.sendFile(path.join(__dirname, 'web/home.html'));
-                            } else res.redirect(401, '/login')
-                        })
+                    let foundPassword = await psReader.getString(tesiEthereumAddress, "password")
+                    if (hash(req.body.password) == foundPassword) {
+                        req.session.logged = true
+                        req.session.username = req.body.username
+                        req.session.address = passports[i].passportAddress
+                        res.sendFile(path.join(__dirname, 'web/home.html'));
+                    } else res.redirect('/login')
 
-                        return
-                    }
-                })
-            }//)
-            //if (!found) res.redirect(401, '/login')
-            setTimeout(() => { if (!found) res.redirect(401, '/login') }, 10000)    // Terribile, ma la riga prima non funziona per asincronicità
+                    break
+                }
+            }
+            if (!found) res.redirect('/login')
         })
-    } else res.redirect(400, '/')
+    } else res.redirect('/')
 })
 
 app.get("/home", (req, res) => {
@@ -231,21 +202,21 @@ app.post("/publishMessage", (req, res) => {     // AJAX method
                         res.status(200).send(/* retrieveAllMessages() */{
                             timestamp: Date.now(),
                             username: req.session.username
-                        })   // Uncommenting would be the best way, but very resource-intensive
+                        })   // Uncommenting would be the best way (??? --> requests overlapped), but very resource-intensive and slow
                     } else {
                         res.sendStatus(500)
                     }
                 })
             })
-        } else res.send(400)
-    } else res.send(403)
+        } else res.sendStatus(400)
+    } else res.sendStatus(403)
 })
 
 app.get("/logout", (req, res) => {
     if (isAuthenticated(req))
         //req.session.destroy()
         req.session.logged = false
-    res.redirect(200, "/")  // Changed from default 301 status code
+    res.redirect("/")
 })
 
 app.get("/retrieveMessages", async (req, res) => {    // Everybody can read without authentication, data is public anyway
@@ -260,54 +231,30 @@ app.listen(port, () => {
     passportReader.getPassportsList(passportFactoryAddress).then(passports => {
         if (passports.length == 0) {
             generator.createPassport(tesiEthereumAddress).then(txConfig => {
-                web3.eth.accounts.signTransaction(txConfig, privateKey).then(signedTx => {
-                    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, receipt) {
-                        if (!error) {
-                            chatHolderAddress = vd.PassportGenerator.getPassportAddressFromReceipt(receipt)
-                            var ownership = new vd.PassportOwnership(web3, chatHolderAddress)
-                            ownership.claimOwnership(tesiEthereumAddress).then(txConf => {
-                                web3.eth.accounts.signTransaction(txConf, privateKey).then(toSubmit => {
-                                    web3.eth.sendSignedTransaction(toSubmit.rawTransaction, function(ownerErr, ownReceipt) {
-                                        if (ownerErr) {
-                                            console.log(ownerErr)
-                                            exit()
-                                        }
-                                        console.log("Startup ended.")
-                                    })
+                sendTransaction(txConfig).then(res => {
+                    chatHolderAddress = vd.PassportGenerator.getPassportAddressFromReceipt(receipt)
+                    var ownership = new vd.PassportOwnership(web3, chatHolderAddress)
+                    ownership.claimOwnership(tesiEthereumAddress).then(txConf => {
+                        sendTransaction(txConf).then(res => {
+                            new vd.Permissions(web3, chatHolderAddress).setWhitelistOnlyPermission(true, tesiEthereumAddress).then(txC => {
+                                sendTransaction(txC).then((rec, err) => {
+                                    if (err) console.log(err)
+                                    else console.log("Startup ended.")
                                 })
-                            }).catch(err => {
-                                console.log(err)
-                                exit()
                             })
-                        } else {
-                            console.log("Error during the submission of the signed transaction, try again!")
-                            console.log(error)
-                            exit()
-                        }
+                        })
+                    }).catch(err => {
+                        console.log(err)
+                        exit()
                     })
-                }).catch(signingError => {
-                    console.log(signingError)
-                    exit()
                 })
             }).catch(err => {
                 console.log(err)
                 exit()
             })
         } else {
-            chatHolderAddress = process.env.CHAT_HOLDER_ADDRESS     // retrieving the list of Passport(s) created and obtaining the first is also valid
+            chatHolderAddress = process.env.CHAT_HOLDER_ADDRESS
             console.log("Digital Identity for chat messages holder already created at " + chatHolderAddress)
-            /*new vd.PassportOwnership(web3, chatHolderAddress).claimOwnership(tesiEthereumAddress).then(txConf => {
-                sendTransaction(txConf).then((rec, err1) => {
-                    if (err1) console.log(err1)
-                    else console.log(rec)
-                })
-            })*/
-            /*new vd.Permissions(web3, chatHolderAddress).setWhitelistOnlyPermission(true, tesiEthereumAddress).then(txConfig => {
-                sendTransaction(txConfig).then((rec, err) => {
-                    if (err) console.log(err)
-                    else console.log(rec)
-                })
-            })*/
             console.log("Startup ended.")
         }
     }).catch(err => {
